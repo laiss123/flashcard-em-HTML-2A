eef81865bccf5ea56d3d87bfcdbc439beaf76815
laiss123 <lais.eufrauzino@escola.pr.gov.br>
2025-10-10 08:04:20 -0300

    mnb.

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criacartao(
+    'Programação'
+    'O que é Python'
+    'O python é uma linguagem de programação'
+)
+criacartao)
+'geografia',
+'qual a capital da frança?',
+'a capital da frança é paris'
+)
+
+criacartao)
+'programação',
+'o que é uma função'
+'uma função é um bloco de código que executa alguma tarefa'
